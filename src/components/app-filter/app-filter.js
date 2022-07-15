5df4d7875d34a2ea42b6b853799b5ac2efb040f1
Ramzan <murdagaliev95@gmail.com>
2022-07-15 19:11:32 +0300

    New project react layout

---
+++ src/components/app-filter/app-filter.js
@@ -0,0 +1,22 @@
+import './app-filter.css'
+
+const AppFilter = () =>{
+    return(
+        <div className="btn-group">
+            <button type='button'
+            className="btn btn-light">
+                Все сотрудники
+            </button>
+            <button type='button'
+            className="btn btn-outline-light">
+                Все сотрудники
+            </button>
+            <button type='button'
+            className="btn btn-outline-light">
+                Все сотрудники
+            </button>
+        </div>
+    )
+}
+
+export default AppFilter
