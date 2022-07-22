13d81448ac3ec5ee375f5947ec15998935460d9d
Ramzan <murdagaliev95@gmail.com>
2022-07-22 17:40:59 +0300

    added events for project for delete and add users

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,32 +1,74 @@
+import { Component } from 'react'
+
 import AppInfo from '../app-info/app-info'
 import SearchPanel from '../search-panel/search-panel'
 import AppFilter from '../app-filter/app-filter'
 import EmployeesList from '../employees-list/employees-list'
 import EmployeesAddForm from '../employees-add-form/employees-add-form'
+
 import './app.css'
 
 
-function App() {
 
-    const data = [
-        {name: 'Jhon S.', salary: 800, premium: true, id:1},
-        {name: 'Alex F.', salary: 1000, premium: false, id:2},
-        {name: 'Jacob L.', salary: 1500, premium: false, id:3}
-    ]
-    return(
-        <div className="app">
-            <AppInfo/>
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Jhon S.', salary: 800, premium: true, id: 1},
+                {name: 'Alex F.', salary: 1000, premium: false, id: 2},
+                {name: 'Jacob L.', salary: 1500, premium: false, id: 3}
+            ]
+        }
+        this.maxId = this.state.data.length + 1
+    }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    deleteItem = (id) => {
+        this.setState(({data}) =>{
+            return{
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    addEmployee = (name, salary) => {
+        if(name && salary) {
+            const newEmployee = {
+                name,
+                salary,
+                premium: false,
+                uplike: false,
+                id: this.maxId};
+            this.setState(({data}) =>{
+                return{
+                    data: [...data, newEmployee]
+                }
+            })
+            this.maxId += 1
+        }
+
+    }
+
+    render(){
+        return(
+            <div className="app">
+                <AppInfo/>
+
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+
+                <EmployeesList
+                data = {this.state.data}
+                onDelete = {this.deleteItem}
+                />
+                <EmployeesAddForm
+                onAddEmployee ={this.addEmployee}/>
+
             </div>
-
-            <EmployeesList data = {data}/>
-            <EmployeesAddForm/>
-
-        </div>
-    )
+        )
+    }
 }
 
 export default App
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -22,9 +22,12 @@ onLike = () =>{
     }))
 }
 
+
 render() {
-    const {name, salary} = this.props;
+    const {name, salary, onDelete} = this.props;
     const {premium, uplike} = this.state;
+
+
     let classNames = "list-group-item d-flex justify-content-between";
     if (premium){
         classNames += ' increase';
@@ -44,8 +47,10 @@ render() {
                     <i className="fas fa-cookie"></i>
                 </button>
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
+                <button
+                        type="button"
+                        className="btn-trash btn-sm "
+                        onClick={onDelete}>
                     <i className="fas fa-trash"></i>
                 </button>
                 <i className="fas fa-star"></i>
